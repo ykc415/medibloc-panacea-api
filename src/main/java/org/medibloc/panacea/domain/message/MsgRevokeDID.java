bfcc4a0a47f892d59f1b17306b65dc8b8b939a45
ykc <pica415@gmail.com>
2020-04-23 14:53:05 +0900

    optimize imports

--- src/main/java/org/medibloc/panacea/domain/message/MsgRevokeDID.java
+++ src/main/java/org/medibloc/panacea/domain/message/MsgRevokeDID.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.medibloc.panacea.domain.did.PubKey;
 
 @NoArgsConstructor
 @Getter @Setter @ToString
